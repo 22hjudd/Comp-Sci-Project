9e627c194b28cb4dbad84277c95b7a23c380a598
Gary Judd <gary.judd@gazsoft.co.uk>
2025-03-13 00:16:34 +0000

    Trying to fix return to menu, about to try some stuff that might break everything

--- PSS_V1.py
+++ PSS_V1.py
@@ -1,61 +1,39 @@
 #setup
 import tkinter as tk #importing tkinter with the abbreviation tk for convinience
 from tkinter import ttk #classes for themed widgets
-import ttkbootstrap as ttkb #more useful tkinter
+import ttkbootstrap as ttkb #more useful tkinter, most used out of the tk libraries
 import pygame #puts the physics on the screen
 import sys #system specific parameters
 import pymunk #does the physics
 import pymunk.pygame_util #draw pymunk objects in pygame
-from pymunk.vec2d import Vec2d #vectors
+from pymunk.vec2d import Vec2d #vectors and positioning, such as mouse pos
 from typing import List #for lists/arrays
-import math
+import math #maths
 
 
 #main code
 
-#setup, next 3 lines are backup window in case im stupid
-#window = tk.Tk() #create window
-#window.title('Menu') #name window
-#window.geometry('600x400') #window size
-MWindow = ttkb.Window(title = 'Menu',
-                     themename = 'darkly', 
-                     size = (600, 400)
-                     ) #make a bootstrap window for more customisation
 
-#creating a grid
-MWindow.columnconfigure((0, 1), weight = 1)
-MWindow.rowconfigure((1, 2), weight = 1)
-
-titleLabel = ttkb.Label(master = MWindow,
-                       text = 'Physics Scenario Simulator Menu',
-                       font = 'Calibri 24 bold',
-                       ) #creates heading
-titleLabel.grid(row = 0,
-               column = 0,
-               columnspan = 2,
-               pady = 20
-               ) #puts the heading on the grid
-
-#WHY DID I PICK THE HARDEST TO DO FIRST
 def menuProjectileButton_func(MWindow):
     MWindow.destroy() #close the menu
 
-    def mainMenuPButton():
-        PWindow.destroy()
+    def toMainMenuPButton_func():
+        global Prun #destroy the projectiles window, used in a button func later
+        Prun = False
     
     def createProjectile():
         global cannonBallBody, cannonBallProp
-        vs = [(20, 0), (0, 20), (20, 20), (0, 20)] #corners of the cannon ball rectangle hitbox. these values may need changing
+        vs = [(0, 0), (20, 0), (20, 20), (0, 20)] #corners of the cannon ball rectangle hitbox, relative to the centre. THESE VALUES MAY NEED CHANGING
 
-        cannonBallBody = pymunk.Body(body_type = pymunk.Body.KINEMATIC) #(mass, inertia,) body type, notes on body type at the bottom
-        
+        cannonBallBody = pymunk.Body(body_type = pymunk.Body.KINEMATIC) #(mass, inertia,) body type. Notes on body type at the bottom
+        #                                                                for dynamic body
         cannonBallProp = pymunk.Poly(cannonBallBody, vs)
         cannonBallProp.collision_type = 1
         cannonBallProp.friction = 0.5 #notes on friction and elasticity values at the bottom
-        cannonBallProp.elasticity = 0.5 #^^^
+        cannonBallProp.elasticity = 0.5 #^ comment
         cannonBallProp.density = 0.1 #prop for properties
         
-        Pspace.add(cannonBallBody) #temp i think
+        Pspace.add(cannonBallBody, cannonBallProp) #add the body of the cannon ball to the space
         return cannonBallBody, cannonBallProp #makes it able to put on screen by returning
 
     def drawProjectile(cannonBallProp):
@@ -71,16 +49,16 @@ def menuProjectileButton_func(MWindow):
 
     def drawCannon(cannonBody):
         if cannonBody:
-            degrees = -math.degrees(cannonBody.angle)  # Convert radians to degrees
+            degrees = -math.degrees(cannonBody.angle)  # Convert radians to degrees, pymunk works in radians but pygame doesn't
             cannonRotation = pygame.transform.rotate(cannonImage, degrees)  # Rotate image
-            cannonRect = cannonRotation.get_rect(center=(int(cannonBody.position.x), int(cannonBody.position.y)))
+            cannonRect = cannonRotation.get_rect(center = (int(cannonBody.position.x), int(cannonBody.position.y)))
             PWindow.blit(cannonRotation, cannonRect)
 
     global PWindow
 
     pygame.init() #initiate pygame
 
-    PWindow = pygame.display.set_mode((1200, 900), pygame.RESIZABLE) #display size
+    PWindow = pygame.display.set_mode((1200, 900), pygame.RESIZABLE) #window size, make window able to resize
     pygame.display.set_caption("Projectiles") #window title
 
     clock = pygame.time.Clock()
@@ -88,7 +66,7 @@ def menuProjectileButton_func(MWindow):
     #FIX THIS LATER
     destoryPWindow = ttkb.Button(master = PWindow,
                                 text = 'Return to main menu',
-                                command = mainMenuPButton
+                                command = toMainMenuPButton_func
                                 )
     destoryPWindow.pack()
 
@@ -97,14 +75,14 @@ def menuProjectileButton_func(MWindow):
         Pspace = pymunk.Space() #creates a space
         Pspace.gravity = (0, 10) #horizontal gravity, vertical gravity
 
-        cannonBallImage = pygame.image.load('cannonBallImage.png')
-        cannonBallImage = pygame.transform.scale(cannonBallImage, (20, 20))
+        cannonBallImage = pygame.image.load('cannonBallImage.png') #load image
+        cannonBallImage = pygame.transform.scale(cannonBallImage, (20, 20)) #scale it up in the x and y direction to be fitting size
 
         cannonImage = pygame.image.load('cannonImage.png')
         cannonImage = pygame.transform.scale(cannonImage, (150, 150))
 
-        cannonBallBody, cannonBallProp = createProjectile()
-        cannonBody = createCannon(Pspace)
+        cannonBallBody, cannonBallProp = createProjectile() #use cannon ball body and properties to create the projectile
+        cannonBody = createCannon(Pspace) #use cannon body to create the cannon in the space
 
         drawSetup = pymunk.pygame_util.DrawOptions(PWindow) #sets up drawing stuff on the screen
 
@@ -136,13 +114,13 @@ def menuProjectileButton_func(MWindow):
             Pspace.debug_draw(drawSetup) #draw the stuff
             fps = 60
             clock.tick(fps)
-            Pspace.step(1/fps) #updates physics simulation loop every 1/fps seconds
+            Pspace.step(1/fps) #updates physics simulation loop every 1/fps (currently 1/60) seconds
             pygame.display.flip() #update entire display
 
     projectileLogic() #runs the main loop + some other stuff when menu button is clicked
 
     pygame.quit() #quit pygame
-    sys.exit() #quit sys
+    menu()
 
 #IDEAS FOR LATER
 #Use a static body as the floor so it interacts better with hitboxes (THIS IS MOSTLY IMPLEMENTED NOW, COMPLICATED)
@@ -176,7 +154,7 @@ def menuAerodynamicButton_func():
         pygame.display.flip()
 
     pygame.quit()
-    sys.exit()
+    menu()
 
 
 def menuOrbitButton_func():
@@ -198,7 +176,7 @@ def menuOrbitButton_func():
         pygame.display.flip()
 
     pygame.quit()
-    sys.exit()
+    menu()
 
 
 def menuOptionsButton_func():
@@ -220,68 +198,95 @@ def menuOptionsButton_func():
         pygame.display.flip()
 
     pygame.quit()
-    sys.exit()
+    menu()
 
 
-menuProjectileButton = ttkb.Button(master = MWindow,
-                                  text = 'Projectiles',
-                                  command = menuProjectileButton_func,
-                                  width = 15,
-                                  bootstyle = 'success-outline'
-                                  ) #Pressing button opens projectiles window, bootstyle outline creates the hover over change colour effect
+def menu():
+    global MWindow
+    #setup, next 3 lines are backup window in case im stupid
+    #window = tk.Tk() #create window
+    #window.title('Menu') #name window
+    #window.geometry('600x400') #window size
+    MWindow = ttkb.Window(title = 'Menu',
+                         themename = 'darkly', 
+                         size = (600, 400)
+                         ) #make a bootstrap window for more customisation
 
-menuAerodynamicButton = ttkb.Button(master = MWindow,
-                                   text = 'Aerodynamics (coming soon)',
-                                   command = menuAerodynamicButton_func,
+    #creating a grid
+    MWindow.columnconfigure((0, 1), weight = 1)
+    MWindow.rowconfigure((1, 2), weight = 1)
+
+    titleLabel = ttkb.Label(master = MWindow,
+                           text = 'Physics Scenario Simulator Menu',
+                           font = 'Calibri 24 bold',
+                           ) #creates heading
+    titleLabel.grid(row = 0,
+                   column = 0,
+                   columnspan = 2,
+                   pady = 20
+                   ) #puts the heading on the grid
+    
+    menuProjectileButton = ttkb.Button(master = MWindow,
+                                      text = 'Projectiles',
+                                      command = menuProjectileButton_func,
+                                      width = 15,
+                                      bootstyle = 'success-outline'
+                                      ) #Pressing button opens projectiles window, bootstyle outline creates the hover over change colour effect
+
+    menuAerodynamicButton = ttkb.Button(master = MWindow,
+                                       text = 'Aerodynamics (coming soon)',
+                                       command = menuAerodynamicButton_func,
+                                       width = 15,
+                                       bootstyle = 'info-outline'
+                                       )
+
+    menuOrbitButton = ttkb.Button(master = MWindow,
+                                 text = 'Orbit (coming soon)',
+                                 command = menuOrbitButton_func,
+                                 width = 15,
+                                 bootstyle = 'light-outline'
+                                 )
+
+    menuOptionsButton = ttkb.Button(master = MWindow,
+                                   text = 'Options (coming soon)',
+                                   command = menuOptionsButton_func,
                                    width = 15,
-                                   bootstyle = 'info-outline'
+                                   bootstyle = 'warning-outline'
                                    )
 
-menuOrbitButton = ttkb.Button(master = MWindow,
-                             text = 'Orbit (coming soon)',
-                             command = menuOrbitButton_func,
-                             width = 15,
-                             bootstyle = 'light-outline'
-                             )
+    menuProjectileButton.grid(row = 1,
+                             column = 0,
+                             sticky = "nsew",
+                             padx = 20,
+                             pady = 20
+                             ) #puts the button into a grid (Arne) slot
 
-menuOptionsButton = ttkb.Button(master = MWindow,
-                               text = 'Options (coming soon)',
-                               command = menuOptionsButton_func,
-                               width = 15,
-                               bootstyle = 'warning-outline'
-                               )
+    menuAerodynamicButton.grid(row = 1,
+                              column = 1,
+                              sticky = "nsew",
+                              padx = 20,
+                              pady = 20
+                              )
 
-menuProjectileButton.grid(row = 1,
-                         column = 0,
-                         sticky = "nsew",
-                         padx = 20,
-                         pady = 20
-                         ) #puts the button into a grid (Arne) slot
+    menuOrbitButton.grid(row = 2,
+                        column = 0,
+                        sticky = "nsew",
+                        padx = 20,
+                        pady = 20
+                        )
 
-menuAerodynamicButton.grid(row = 1,
+    menuOptionsButton.grid(row = 2,
                           column = 1,
                           sticky = "nsew",
                           padx = 20,
                           pady = 20
                           )
 
-menuOrbitButton.grid(row = 2,
-                    column = 0,
-                    sticky = "nsew",
-                    padx = 20,
-                    pady = 20
-                    )
 
-menuOptionsButton.grid(row = 2,
-                      column = 1,
-                      sticky = "nsew",
-                      padx = 20,
-                      pady = 20
-                      )
+    #run the menu
+    MWindow.mainloop() #run the window
 
-
-#run the menu
-MWindow.mainloop() #run the window
+menu()
 
 #notes on pymunk
 
